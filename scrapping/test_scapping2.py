f527314cdd96524ad78162ddb320d6792d0991ae
Sylvain Courtin <sylvain.courtin1@etu.univ-orleans.fr>
2018-01-25 17:11:35 +0100

    synchro

--- scrapping/test_scapping2.py
+++
@@ -1,9 +0,0 @@
-import requests
-res = requests.get('https://www.facebook.com/frank.candido.5')
-res.raise_for_status()
-playFile = open('test.txt', 'wb')
-
-for chunk in res.iter_content(100000):
-    playFile.write(chunk)
-
-playFile.close()
