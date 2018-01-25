34ffa91b495d43a040c737e5ba34ce47e478a74e
Sylvain Courtin <sylvain.courtin1@etu.univ-orleans.fr>
2018-01-25 10:54:29 +0100

    merge

--- scrapping/linkedin/main.py
+++ scrapping/linkedin/main.py
@@ -7,6 +7,7 @@ import argparse
 import time
 import os
 import bs4
+import platform
 
 def parse_command_line_args():
     parser = argparse.ArgumentParser(description="""
@@ -70,92 +71,106 @@ if __name__ == "__main__":
     search_keys = parse_command_line_args()
 
     # initialize selenium webdriver - pass latest chromedriver path to webdriver.Chrome()
-    driver = webdriver.Firefox(executable_path=os.getcwd()+"/geckodriver_windows64.exe")
-    driver.get("https://www.linkedin.com/uas/login")
+    #On gere ici si l'os qui lance l'appli est windows ou Linux
+    #TODO Mac si besoin, il faudra par contre telecharger le driver de mac
+    os_driver = "error"
+    if platform.system().find("Windows") != -1 :
+        os_driver = "/geckodriver_windows64.exe"
+    elif platform.system().find("Linux") != -1 :
+        os_driver = "/geckodriver_linux"
+    else :
+        print("OS non supporté")
+        os_driver = "error"
 
-    # initialize LinkedIn web client
-    liclient = LIClient(driver, **search_keys)
+    if os_driver != "error" :
+        driver = webdriver.Firefox(executable_path=os.getcwd()+os_driver)
+        driver.get("https://www.linkedin.com/uas/login")
 
-    liclient.login()
+        # initialize LinkedIn web client
+        liclient = LIClient(driver, **search_keys)
 
-    # wait for page load
-    time.sleep(3)
+        liclient.login()
 
-    profile_link="https://www.linkedin.com/in/marcbenioff/fr"
-    driver.get(profile_link)
+        # wait for page load
+        time.sleep(3)
 
-    # wait for page load
-    time.sleep(3)
+        profile_link="https://www.linkedin.com/in/marcbenioff/fr"
+        driver.get(profile_link)
 
-    html=driver.page_source
+        # wait for page load
+        time.sleep(3)
 
-    file=open('scrapping.log', 'w')
+        html=driver.page_source
 
-    soup=bs4.BeautifulSoup(html, "html.parser") #specify parser or it will auto-select for you
+        file=open('scrapping.log', 'w')
+
+        soup=bs4.BeautifulSoup(html, "html.parser") #specify parser or it will auto-select for you
 
 
-    valeurs = soup.find_all('section', class_='experience-section')
-    if len(valeurs)>0:
-        file.write('Données:\n')
-        for elem in valeurs:
-            if(elem.get_text() != ''):
-                file.write(elem.get_text())
+        valeurs = soup.find_all('section', class_='experience-section')
+        if len(valeurs)>0:
+            file.write('Données:\n')
+            for elem in valeurs:
+                if(elem.get_text() != ''):
+                    file.write(elem.get_text())
+                    file.write('\n')
+        else:
+            file.write('- no data -\n')
+        
+        """valeurs = soup.select('.background-details .pv-profile-section__card-item .pv-entity__summary-info')
+        if len(valeurs)>0:
+            print('Valeurs:')
+            for elem in valeurs:
+                print(elem)
+        else:
+            print('- no data -')"""
+
+        """a=0
+        valeurs = soup.select('.background-details .pv-profile-section .experience-section .pv-profile-section__card-item .pv-entity__summary-info span')
+        if len(valeurs)>0:
+            file.write('Experience:\n')
+            for elem in valeurs:
+                a=a+1
+                if a%2==0:
+                    file.write(elem.getText())
+                    file.write('\n')
+        else:
+            file.write('- no data -\n')
+        file.write('--------------------------------------------------\n')
+
+        valeurs = soup.select('.background-details .pv-profile-section .experience-section .pv-profile-section__card-item .pv-entity__summary-info h3')
+        if len(valeurs)>0:
+            file.write('Experience:\n')
+            for elem in valeurs:
+                file.write(elem.getText())
                 file.write('\n')
-    else:
-        file.write('- no data -\n')
+        else:
+            file.write('- no data -\n')
+        file.write('--------------------------------------------------\n')
+
+        a=0
+        valeurs = soup.select('.background-details .pv-profile-section .education-section .pv-profile-section__card-item .pv-entity__summary-info span')
+        if len(valeurs)>0:
+            file.write('Formation:\n')
+            for elem in valeurs:
+                a=a+1
+                if a%2==0:
+                    file.write(elem.getText())
+                    file.write('\n')
+        else:
+            file.write('- no data -\n')
+        file.write('--------------------------------------------------\n')
+
+        valeurs = soup.select('.background-details .pv-profile-section .education-section .pv-profile-section__card-item .pv-entity__summary-info h3')
+        if len(valeurs)>0:
+            file.write('Formation:\n')
+            for elem in valeurs:
+                file.write(elem.getText())
+                file.write('\n')
+        else:
+            file.write('- no data -\n')"""
+        file.write('--------------------------------------------------\n')
+        file.close()
+        liclient.driver_quit()
     
-    """valeurs = soup.select('.background-details .pv-profile-section__card-item .pv-entity__summary-info')
-    if len(valeurs)>0:
-        print('Valeurs:')
-        for elem in valeurs:
-            print(elem)
-    else:
-        print('- no data -')"""
-
-    """a=0
-    valeurs = soup.select('.background-details .pv-profile-section .experience-section .pv-profile-section__card-item .pv-entity__summary-info span')
-    if len(valeurs)>0:
-        file.write('Experience:\n')
-        for elem in valeurs:
-            a=a+1
-            if a%2==0:
-                file.write(elem.getText())
-                file.write('\n')
-    else:
-        file.write('- no data -\n')
-    file.write('--------------------------------------------------\n')
-
-    valeurs = soup.select('.background-details .pv-profile-section .experience-section .pv-profile-section__card-item .pv-entity__summary-info h3')
-    if len(valeurs)>0:
-        file.write('Experience:\n')
-        for elem in valeurs:
-            file.write(elem.getText())
-            file.write('\n')
-    else:
-        file.write('- no data -\n')
-    file.write('--------------------------------------------------\n')
-
-    a=0
-    valeurs = soup.select('.background-details .pv-profile-section .education-section .pv-profile-section__card-item .pv-entity__summary-info span')
-    if len(valeurs)>0:
-        file.write('Formation:\n')
-        for elem in valeurs:
-            a=a+1
-            if a%2==0:
-                file.write(elem.getText())
-                file.write('\n')
-    else:
-        file.write('- no data -\n')
-    file.write('--------------------------------------------------\n')
-
-    valeurs = soup.select('.background-details .pv-profile-section .education-section .pv-profile-section__card-item .pv-entity__summary-info h3')
-    if len(valeurs)>0:
-        file.write('Formation:\n')
-        for elem in valeurs:
-            file.write(elem.getText())
-            file.write('\n')
-    else:
-        file.write('- no data -\n')"""
-    file.write('--------------------------------------------------\n')
-    file.close()
-    liclient.driver_quit()
+    
--- scrapping/linkedin/main2.py
+++ scrapping/linkedin/main2.py
@@ -7,6 +7,7 @@ import argparse
 import time
 import os
 import bs4
+import platform
 
 def parse_command_line_args():
     parser = argparse.ArgumentParser(description="""
@@ -70,42 +71,61 @@ if __name__ == "__main__":
     search_keys = parse_command_line_args()
 
     # initialize selenium webdriver - pass latest chromedriver path to webdriver.Chrome()
-    driver = webdriver.Firefox(executable_path=os.getcwd()+"/geckodriver_windows64.exe")
-    driver.get("https://www.linkedin.com/uas/login")
 
-    # initialize LinkedIn web client
-    liclient = LIClient(driver, **search_keys)
+    #On gere ici si l'os qui lance l'appli est windows ou Linux
+    #TODO Mac si besoin, il faudra par contre telecharger le driver de mac
+    os_driver = "error"
+    if platform.system().find("Windows") != -1 :
+        os_driver = "/geckodriver_windows64.exe"
+    elif platform.system().find("Linux") != -1 :
+        os_driver = "/geckodriver_linux"
+    else :
+        print("OS non supporté")
+        os_driver = "error"
 
-    liclient.login()
+    if os_driver != "error" :
+        driver = webdriver.Firefox(executable_path=os.getcwd()+"/geckodriver_windows64.exe")
+        driver.get("https://www.linkedin.com/uas/login")
 
-    # wait for page load
-    time.sleep(3)
+        # initialize LinkedIn web client
+        liclient = LIClient(driver, **search_keys)
 
-    profile_link="https://www.linkedin.com/search/results/people/?firstName=sylvain&lastName=courtin"
+        liclient.login()
 
-    driver.get(profile_link)
+        # wait for page load
+        time.sleep(3)
 
-    # wait for page load
-    time.sleep(3)
+        #Pour l'instant en dur, on pourra imaginer les différents arg de la recherche en parametre de la fonction
+        nom = "candido"
+        prenom = "frank"
+        #
+        
+        recherche_nom= "lastName="
+        recherche_prenom = "firstName="
+        profile_link="https://www.linkedin.com/search/results/people/?"+recherche_nom+nom+"&"+recherche_prenom+prenom
 
-    file=open('main2.py.log','w')
+        driver.get(profile_link)
 
-    html=driver.page_source
-    soup=bs4.BeautifulSoup(html, "html.parser")
+        # wait for page load
+        time.sleep(3)
 
-    #same = soup.select(".results-list .ember-view")
-    #same = soup.find_all('a')
-    same=soup.find_all('a', class_='search-result__result-link')
-    try:
-        # le nbr de résultat est doublé car, pour une personne nous avons deux lien pour rejoindre son profil (click sur son image de profil + nom)
-        a=0
-        for elem in same:
-            a=a+1
-            if a%2==0:
-                file.write('https://www.linkedin.com')
-                file.write(elem.get('href'))
-                file.write('\n')
-    except Exception as e:
-        print(e)
-    file.close()
-    liclient.driver_quit()
+        file=open('main2.py.log','w')
+
+        html=driver.page_source
+        soup=bs4.BeautifulSoup(html, "html.parser")
+
+        same=soup.find_all('a', class_='search-result__result-link')
+        try:
+            # le nbr de résultat est doublé car, pour une personne nous avons deux lien pour rejoindre son profil (sur son image de profil + nom)
+            a=0
+            for elem in same:
+                a=a+1
+                if a%2==0:
+                    #le lien est coupé, on rajoute donc ceci au début de chaque ligne
+                    file.write('https://www.linkedin.com')
+                    file.write(elem.get('href'))
+                    file.write('\n')
+        except Exception as e:
+            print(e)
+        file.close()
+        #liclient.driver_quit()
