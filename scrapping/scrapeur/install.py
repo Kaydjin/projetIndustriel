cca4d70983fa5024a27188de73b4491275bed8b2
Unknown <bryce.marbois@laposte.net>
2018-02-19 15:08:16 +0100

    ajout de download de libraries pour nltk

---
+++ scrapping/scrapeur/install.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+nltk.download('maxent_ne_chunker')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('gutenberg')
+nltk.download('words')
