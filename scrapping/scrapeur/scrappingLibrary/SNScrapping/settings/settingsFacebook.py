dd9c800c0af554049da3993b1faf69726e0c018d
VogelJimmy <jimmy.vogel@etu.univ-orleans.fr>
2018-03-01 19:52:04 -0100

    mise a jour settings facebook

--- scrapping/scrapeur/scrappingLibrary/SNScrapping/settings/settingsFacebook.py
+++ scrapping/scrapeur/scrappingLibrary/SNScrapping/settings/settingsFacebook.py
@@ -29,11 +29,77 @@ search_keys = (
     "results_page"     :  ""
     },
     {
-    "username"         :  "testgeo2@laposte.net",
-    "password"         :  "Testgeo2",
+    "username"         :  "testgeo3@laposte.net",
+    "password"         :  "Testgeo3",
     "keywords"         :  ["Data Scientist", "Data Analyst"],
     "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
 
+    "search_radius"    :  "50",
+    "page_number"      :  1,
+    "date_range"       :  "All",
+    "sort_by"          :  "Date Posted",
+    "salary_range"     :  "All",
+    "filename"         :  "output.txt",
+    "results_page"     :  ""
+    }
+    {
+    "username"         :  "testgeo4@laposte.net",
+    "password"         :  "Testgeo4",
+    "keywords"         :  ["Data Scientist", "Data Analyst"],
+    "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
+    "search_radius"    :  "50",
+    "page_number"      :  1,
+    "date_range"       :  "All",
+    "sort_by"          :  "Date Posted",
+    "salary_range"     :  "All",
+    "filename"         :  "output.txt",
+    "results_page"     :  ""
+    },
+    {
+    "username"         :  "testgeo5@laposte.net",
+    "password"         :  "Testgeo5",
+    "keywords"         :  ["Data Scientist", "Data Analyst"],
+    "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
+    "search_radius"    :  "50",
+    "page_number"      :  1,
+    "date_range"       :  "All",
+    "sort_by"          :  "Date Posted",
+    "salary_range"     :  "All",
+    "filename"         :  "output.txt",
+    "results_page"     :  ""
+    },
+    {
+    "username"         :  "testgeo6@laposte.net",
+    "password"         :  "Testgeo6",
+    "keywords"         :  ["Data Scientist", "Data Analyst"],
+    "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
+
+    "search_radius"    :  "50",
+    "page_number"      :  1,
+    "date_range"       :  "All",
+    "sort_by"          :  "Date Posted",
+    "salary_range"     :  "All",
+    "filename"         :  "output.txt",
+    "results_page"     :  ""
+    },
+    {
+    "username"         :  "testgeo7@laposte.net",
+    "password"         :  "Testgeo7",
+    "keywords"         :  ["Data Scientist", "Data Analyst"],
+    "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
+    "search_radius"    :  "50",
+    "page_number"      :  1,
+    "date_range"       :  "All",
+    "sort_by"          :  "Date Posted",
+    "salary_range"     :  "All",
+    "filename"         :  "output.txt",
+    "results_page"     :  ""
+    },
+    {
+    "username"         :  "testgeo8@laposte.net",
+    "password"         :  "Testgeo8",
+    "keywords"         :  ["Data Scientist", "Data Analyst"],
+    "locations"        :  ["San Francisco Bay Area", "Greater New York City Area"],
     "search_radius"    :  "50",
     "page_number"      :  1,
     "date_range"       :  "All",
