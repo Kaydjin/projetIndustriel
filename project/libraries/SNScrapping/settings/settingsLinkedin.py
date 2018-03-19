c52d3805ff396ade6732aedf454d44fc8187d750
Unknown <bryce.marbois@laposte.net>
2018-03-19 16:43:24 +0100

    majs compte

--- project/libraries/SNScrapping/settings/settingsLinkedin.py
+++ project/libraries/SNScrapping/settings/settingsLinkedin.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 search_keys = (
-        {
+    {
     "username"         : "bob.marnier@laposte.net",
     "password"         : "BobMarnier1"
     },
@@ -11,32 +11,32 @@ search_keys = (
     "password"         :  "Alisa.marnier1"
     },
     {
-    "username"         :  "testgeo3@laposte.net",
-    "password"         :  "Testgeo3"
+    "username"         : "bob.marnier@laposte.net",
+    "password"         : "BobMarnier1"
+    },
+    {
+    "username"         :  "alisa.marnier@laposte.net",
+    "password"         :  "Alisa.marnier1"
+    },
+   {
+    "username"         : "bob.marnier@laposte.net",
+    "password"         : "BobMarnier1"
     },
     {
     "username"         :  "alisa.marnier@laposte.net",
     "password"         :  "Alisa.marnier1"
     },
     {
-    "username"         :  "testgeo6@laposte.net",
-    "password"         :  "testgeo6"
+    "username"         : "bob.marnier@laposte.net",
+    "password"         : "BobMarnier1"
     },
     {
     "username"         :  "alisa.marnier@laposte.net",
     "password"         :  "Alisa.marnier1"
     },
     {
-    "username"         :  "testgeo7@laposte.net",
-    "password"         :  "Testgeo7"
-    },
-    {
-    "username"         :  "alisa.marnier@laposte.net",
-    "password"         :  "Alisa.marnier1"
-    },
-    {
-    "username"         :  "testgeo4@laposte.net",
-    "password"         :  "Testgeo4"
+    "username"         : "bob.marnier@laposte.net",
+    "password"         : "BobMarnier1"
     },
     {
     "username"         :  "alisa.marnier@laposte.net",
